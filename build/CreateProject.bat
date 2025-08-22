f70559affd7f0a608d33b8fe66540989504769a7
LionAnt <nickreal03@gmail.com>
2025-08-23 02:22:16 +0800

    Updated the build system

--- build/CreateProject.bat
+++ build/CreateProject.bat
@@ -1,3 +1,4 @@
 rmdir /s /q xbmp_unit_test.vs2022
-cmake . -G "Visual Studio 17 2022" -A x64 -B xbmp_unit_test.vs2022
+rmdir /s /q ..\dependencies
+cmake ../ -G "Visual Studio 17 2022" -A x64 -B xbmp_unit_test.vs2022
 pause
